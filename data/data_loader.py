7f6df86e9d619eb183e246feaa33ef32276880c1
Alpha-Kittens <90939705+Alpha-Kittens@users.noreply.github.com>
2022-10-02 14:56:48 -0400

    added data loader

---
+++ data/data_loader.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+def read_data(file_path):
+    
+    data = np.array([[0,0]])
+
+    #print ("starting to read")
+    
+    with open(file_path) as f:
+        
+        for row in f.readlines():
+
+            line = row.split("\t")
+
+            print (line)
+
+            data = np.append(data, [[float(line[1]), float(line[2])]], axis = 0)
+
+    return data[1:]
+
+if __name__ == "__main__":
+
+    filepath = "./day_1/calibration_scan_1_3800_4600"
+
+    print(read_data(filepath))
+
