a51dd47559f3531a1910f16ef43811086fa9fd1a
Alpha-Kittens <90939705+Alpha-Kittens@users.noreply.github.com>
2022-10-02 15:43:42 -0400

    Starting framework on calibration.py, added /references for pdfs and hyperlinks

--- calibration.py
+++ calibration.py
@@ -1,3 +1,42 @@
-#from lorenzian_fit import *
+import import_ipynb
+import lorentzian_fit
+
+from data.data_loader import read_data
 from lmfit import Model, Parameter, fit
+
+reference = {
+    'mercury' : {
+        'reference' : [],
+        'check' : [],
+    },
+
+}
+
+def get_band(file):
+    """
+    Given file, runs Athira's lorenzian_fit in order to extract a mean and "scale" (~width) 
+    """
+    
+    raise NotImplementedError
+    return mean, scale
+
+
+def get_calibration_data(files):
+    """
+    Given list of files (consdier: element name, and it just knows the files), extracts the band details from each of them. 
+    Returs centers and weights
+    """
+
+    raise NotImplementedError
+
+def fit_calibration(files, element):
+
+    raise NotImplementedError
+
+def check_calibration(c_function, references):
+    """
+    Given a list of smaller-line calibration references, checks validity of fit. 
+    """
+
+    raise NotImplementedError
 
