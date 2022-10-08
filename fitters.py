22ee6a312c12a1ce8f154a1f877eec8c8b3afc78
ButterflyNebula <athira.arayath@gmail.com>
2022-10-08 18:27:26 -0400

    docstring for fitters.py
    currently not implented

---
+++ fitters.py
@@ -0,0 +1,25 @@
+#Imports
+import numpy as np
+import csv
+import math
+import matplotlib.pyplot as plt
+import scipy.stats as stats
+from scipy.stats import norm, chi2 # normal distribution, chi squared distribution
+from numpy import exp, pi, sqrt
+import lmfit
+
+def fit(model, data, weights):
+    """
+    implements lmfit to fit given data to a specified model
+
+    Arguments: 
+        * `model` (Model): lmfit Model to fit to
+        * `data` (nx2 numpy array): numpy array (matrix) with 2 columns:
+            - First column is x-axis (input to Model)
+            - Second column is y-axis (output of Model)
+        * `weights` (n dimensional array) weights for each data point (x,y) in data
+
+    Returns: 
+        * `result` (ModelResult) result of fit (returned from model.fit)
+    """
+    raise NotImplementedError
