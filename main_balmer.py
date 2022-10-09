aac1c31f05c035ceab8b99dbe7fe77f0a7dc4041
ButterflyNebula <athira.arayath@gmail.com>
2022-10-09 09:03:22 -0400

    added files to main_isotope_shift
    ^^

--- main_balmer.py
+++ main_balmer.py
@@ -3,7 +3,6 @@
 
 # Read data
 
-
 # Noise reduction and find Uncertainties
 
 
--- main_isotopeShift.py
+++ main_isotopeShift.py
@@ -1,14 +1,31 @@
-# File Path to peak data files
+# Imports
+from data import data_loader
+import lmfit
+from fitters import fit
 
+# File Path to peak data files
+folder = 'data/DeuteriumScans/SuperFineScans'
+
+alpha_L_fp  = folder + '/alpha5L'
+alpha_H_fp  = folder + '/alpha4H'
+
+beta_L_fp = folder + '/beta1L'
+beta_H_fp = folder + '/beta2H'
 
 # Read data
+data = data_loader.read_data(beta_L_fp)
 
 
 # Noise reduction and find Uncertainties
 
 
 # Voigt Model & Fit
+model = lmfit.models.VoigtModel()
 
+
+result = fit(model, data)
+
+lmfit.report_fit(result)
 
 # Maximum Counts
 
