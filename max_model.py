c1dfef5becb08f3056b15d688ba911f350d37f59
Alpha-Kittens <90939705+Alpha-Kittens@users.noreply.github.com>
2022-10-09 13:11:46 -0400

    max_model and noise_reduction updates
    added naive cutoff determination method, added naive noise reduction method. Max method is yet to be implemented.

--- max_model.py
+++ max_model.py
@@ -1,4 +1,6 @@
-
+import numpy as np
+from data.data_loader import read_data
+import matplotlib.pyplot as plt
 
 def get_max(data, weights):
     """
@@ -15,3 +17,123 @@ def get_max(data, weights):
     """
 
     raise NotImplementedError
+
+def regions(cps, weights, reduce = True):
+
+    cutoff = get_cutoff(cps, weights)
+    return get_regions(cps, cutoff, reduce = reduce)
+
+def get_regions(cps, cutoff, reduce = False):
+
+    background_regions = []
+    signal_regions = []
+
+    data_below = cps <= cutoff
+
+    #print (data_below)
+    
+    tracking_background = True
+    start = 0
+
+    for i in range(len(cps)):
+
+        if data_below[i] != tracking_background:
+            if tracking_background:
+                if (not reduce or (i-1) - start > 1) and i-1 >= 0:
+                    background_regions.append((start, i-1))
+            else:
+                if not reduce or (i-1) - start > 1:
+                    signal_regions.append((start, i-1))
+            tracking_background = not tracking_background
+            start = i
+
+    if tracking_background:
+        background_regions.append((start, len(cps) - 1))
+    else:
+        signal_regions.append((start, len(cps) - 1)) 
+
+
+    return background_regions, signal_regions
+
+
+
+def get_cutoff(cps, weights):
+    """
+    Given an array of positive values and weights, determines a cutoff do distinguish signal from noise. This algorithm is not particularly refined.
+    
+    Arguments:
+        * `cps` (np array of positive values): Array for which cutoff is to be chosen
+        * `weights` (np array of same dimension): Weights corresponding to `cps` values. Unused by method. 
+    Returns:
+        * `cutoff` (float): signal/noise cutoff for `cps` array. 
+    """
+
+    #print(np.histogram(cps, max(cps) // min(cps)))
+
+    #print (int(max(cps) // min(cps)))
+
+    print (max(cps))
+    print (min(cps))
+
+    print (int(max(cps) // min(cps)))
+
+    hist, binedges = np.histogram(cps, int(max(cps) // min(cps)))
+
+    print (hist, "\n", binedges)
+
+    #print (hist)
+    #print (binedges)
+
+    cutoff = 1 + int(max(cps) // min(cps) / 100)
+
+    print ("The cutoff is ", binedges[cutoff], ". ", np.sum(hist[cutoff:]), "/", np.sum(hist), " of the data is above the cutoff.")
+
+    bincenters = (binedges[1:] + binedges[:-1]) / 2
+    #plt.bar(bincenters, hist, width = (binedges[-1] - binedges[0]) / len(hist))
+    #plt.show()
+
+    return binedges[cutoff]
+
+    #return hist, binedges
+
+if __name__ == "__main__":
+
+    data = read_data("data/DeuteriumScans/SuperFineScans/alpha4H")
+    
+    x, y = data[:,0], data[:,1]
+
+    ysize = y.size
+    xsize = x.size
+    step = (max(x) - min(x) + 1)/xsize
+    #print(ysize)
+    #print(xsize)
+    #print(step)
+    from scipy.fft import fft, ifft, fftfreq
+    frequencies, amplitudes = fftfreq(ysize, step), fft(y)
+
+    cps = np.abs(amplitudes)
+    #cps = data[:,1]
+    print (max(cps))
+    cutoff = get_cutoff(cps, 0)
+    print (get_regions(cps, cutoff, reduce = True))
+    plt.plot(frequencies, np.abs(amplitudes))
+    plt.plot(frequencies, np.where(np.abs(amplitudes) > cutoff, np.abs(amplitudes), -1))
+
+    amplitudes_new = np.where(np.abs(amplitudes) > cutoff, amplitudes, 0)
+    #plt.plot(data[:,0], data[:,1])
+    #plt.plot(data[:,0], np.where(cps > cutoff, cps, -1))
+    plt.show()
+    #hist, binedges = get_cutoffs(data, 0)
+    #bincenters = (binedges[1:] + binedges[:-1]) / 2
+    #print (bincenters)
+    plt.plot(x, y)
+    plt.plot(x, ifft(amplitudes_new))
+    plt.show()
+    #print (hist)
+    #print (bincenters)
+    
+    #plt.bar(bincenters, hist, width = (binedges[-1] - binedges[0]) / len(hist))
+    #plt.show()
+
+    #plt.plot()
+    #plt.show()
--- noise_reduction.py
+++ noise_reduction.py
@@ -1,4 +1,7 @@
 import numpy as np
+from scipy.fft import fft, ifft, fftfreq
+import matplotlib.pyplot as plt 
+from data.data_loader import read_data
 
 def reduce_noise (data, cutoff = 5):
     """
@@ -15,4 +18,112 @@ def reduce_noise (data, cutoff = 5):
             - `weights` (numpy array length n): weights associated with each new data point.  
     """
 
-    raise NotImplementedError
+    x, y = data[:,0], data[:,1]
+
+    ysize = y.size
+    xsize = x.size
+    step = (max(x) - min(x) + 1)/xsize
+    #print(ysize)
+    #print(xsize)
+    #print(step)
+
+    frequencies, amplitudes = fftfreq(ysize, step), fft(y)
+
+    #print (frequencies)
+
+    #plot(x, y, "Data", "Wavelength (A)", "CPS")
+
+    #print(amplitudes)
+    #print(np.abs(amplitudes))
+
+    #plot(frequencies, np.abs(amplitudes), "FFT of data", "frequency", "FFT")
+
+    new_amplitudes, removed_amplitudes = bandpass(frequencies, amplitudes)
+
+    #plot(x, ifft(removed_amplitudes), "Removed stuff", "Wavelength", "CPS", show = False)
+    #plot(x, np.abs(ifft(removed_amplitudes)), "Removed stuff", "Wavelength", "CPS")
+
+    new_data = ifft(new_amplitudes)
+    removed = ifft(removed_amplitudes)
+
+    #plot(x, y, "Data", "Wavelength (A)", "CPS", show = False)
+    #plot(x, new_data, "IFFT data", "Wavelength (A)", "CPS", show = True)
+
+    #raise NotImplementedError
+
+    neighborhood = int(len(x) // 40)
+
+    errors = [np.sqrt(np.sum(removed[max(i - neighborhood, 0):min(i + neighborhood + 1, len(removed))] ** 2) / (min(i + neighborhood + 1, len(removed)) - max(i - neighborhood, 0))) for i in range(len(removed))]
+    print (errors)
+    #plt.plot(x, errors)
+    plt.errorbar(x, new_data, errors)
+    plt.plot(x, y)
+    plt.show()
+
+    return new_data, 1/errors
+
+def bandpass(frequencies, amplitudes):
+    """
+    Given frequencies and amplitudes, uses the cutoff method from max_model to retain the dominant frequencies and drop the higher frequencies, which mostly contribute to noise.
+    Will consider adding an exponential damping in the future. 
+
+    Arguments:
+        * `frequencies` (numpy array): Frequencies of fourier transform of data.
+        * `amplitudes` (numpy array): Amplitudes of fourier transform of data. Indices correspond with `frequencies`. 
+
+    Returns:
+        * As a 2-tuple:
+            `new_amplitudes` (numpy array of same dimension as `amplitudes`): Array of new amplitudes. 
+            `removed_amplitudes` (numpy array of same dimension as `amplitudes`): Array of removed amplitudes. These two should sum to the original. 
+    """
+
+    #consider: exponential damping of unused amplitudes. 
+
+    from max_model import get_cutoff
+
+    cutoff = get_cutoff(np.abs(amplitudes), 0)
+
+    return np.where(np.abs(amplitudes) > cutoff, amplitudes, 0), np.where(np.abs(amplitudes) <= cutoff, amplitudes, 0)
+
+
+def bandpass_old(frequencies, amplitudes):
+
+    #use max_model to find width?
+
+    from max_model import regions
+
+    backgrounds, signals = regions(np.abs(amplitudes), 0, reduce = False)
+
+    print(regions(np.abs(amplitudes), 0, reduce = False))
+
+    # rms of cosine is 1/sqrt(2)
+
+    cutoff = 0.1
+
+    fmin = min(frequencies)
+    span = max(frequencies) - min(frequencies)
+    lower = fmin + span * (1-cutoff)/2
+    upper = fmin + span * (1+cutoff)/2
+
+    #indices = [frequency > lower and frequency < upper for frequency in frequencies]
+    signal_indices = []
+    for signal in signals:
+        signal_indices.append(range(signal[0], signal[1]+1))
+    indices = [i in signal_indices for i in range(len(amplitudes))]
+
+    return frequencies, np.where(indices, amplitudes, 0)
+
+
+def plot(x, y, title, xlabel, ylabel, show = True):
+
+    plt.plot(x, y)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.title(title)
+    if show:
+        plt.show()
+
+if __name__ == "__main__":
+    file = "data/DeuteriumScans/SuperFineScans/alpha4H"
+    data = read_data(file)
+    reduce_noise(data)
