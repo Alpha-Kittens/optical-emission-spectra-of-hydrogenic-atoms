98379abdb58f2367a76566e962eed8f37495b335
ButterflyNebula <athira.arayath@gmail.com>
2022-10-13 22:14:36 -0400

    finsihed isotope shift
    ^^added uncertainties and calculation of deuterium to hydrogen mass ratio

--- main_isotopeShift.py
+++ main_isotopeShift.py
@@ -5,6 +5,7 @@ from fitters import fit
 import matplotlib.pyplot as plt
 import models
 import noise_reduction
+import math
 
 # File Path to peak data files
 folder = 'data/DeuteriumScans/SuperFineScans'
@@ -14,29 +15,40 @@ alpha_H_fp  = folder + '/alpha4H'
 beta_L_fp = folder + '/beta1L'
 beta_H_fp = folder + '/beta2H'
 
-gamma_L_fp = folder + '/gamma1L'
-gamma_H_fp = folder + '/gamma2H'
+gamma_L_fp = folder + '/gamma2L'
+gamma_H_fp = folder + '/gamma1H'
 
 delta_L_fp = folder + '/delta1L'
 delta_H_fp = folder + '/delta2H'
+
+
+#Constants
+m_p = 1.67262192E-27 #kilograms
+m_e = 9.1093837E-31 #kilograms
+nf = 2
+R_inf = 10973731.568160 #m^-1
 
 # Wavelengths
 alpha = {
     "1_fp" : alpha_L_fp,
     "2_fp" : alpha_H_fp,
+    "ni"   : 3,
 }
 beta = {
     "1_fp" : beta_L_fp,
     "2_fp" : beta_H_fp,
+    "ni"   : 4,
 }
-'''
 gamma = {
     "1_fp" : gamma_L_fp,
     "2_fp" : gamma_H_fp,
+    "ni"   : 5,
 }
+'''
 delta = {
     "1_fp" : delta_L_fp,
     "2_fp" : delta_H_fp,
+    "ni"   : 6,
 }
 '''
 
@@ -45,13 +57,13 @@ delta = {
 information = {
     'alpha' : alpha,
     'beta' : beta,
-    #'gamma' : gamma,
+    'gamma' : gamma,
     #'delta' : delta,
 }
 damping_constants = {
     'alpha' : 1/10,
     'beta' : 1/3,
-    #'gamma' : 1/10,
+    'gamma' : 1/10,
     #'delta' : 1/10
 }
 
@@ -96,13 +108,66 @@ for key in information:
 
         wavelength[str(i) + "_result"] = result
 
-        # Maximum Counts
+        # Maximum Counts Method
+    
+    # Calibration to convert the wavelengths
 
     # Isotope shifts (deuterium wavelength - hydrogen wavelength)
     wavelength["shift"] = abs(wavelength["1_result"].best_values['center'] - wavelength["2_result"].best_values['center'])
 
+    # Uncertainty in Shift
+    wavelength["shift_unc"] = math.sqrt((wavelength["1_result"].params['center'].stderr)**2 + (wavelength["1_result"].params['center'].stderr)**2)
 
-# Balmer Formula to find ratio of deuterium to proton mass
+    # Balmer Formula to find ratio of deuterium to proton mass
+    wavelength["ratio"] = 1/(1 - R_inf*(m_p/m_e)*wavelength["shift"]*(10**(-10))*((1/(nf**2)) - (1/(wavelength["ni"]**2))))
+
+    wavelength["ratio_unc_square"] = (wavelength["shift"]*(10**(-10))*((1/(nf**2)) - (1/(wavelength["ni"]**2)))*wavelength["shift_unc"]**2)/(wavelength["ratio"]**2)
 
 
-# Average and Standard Dev.
+# Mean
+sum = 0
+for key in information:
+    wavelength = information[key]
+    print("ratio: " + str(wavelength["ratio"]))
+    sum += wavelength["ratio"]
+
+mean = sum/len(information)
+print(mean)
+
+# Statistical Uncertainty
+sum = 0
+for key in information:
+    wavelength = information[key]
+    sum += (wavelength["ratio"] - mean)**2
+
+std_dev = math.sqrt(sum/(len(information) - 1))
+
+stat_unc = std_dev/math.sqrt(len(information))
+
+
+# Systematic Uncertainty
+sum = 0
+for key in information:
+    wavelength = information[key]
+    sum += (wavelength["shift_unc"])**2
+total_shift_unc = math.sqrt(sum)/math.sqrt(len(information))
+print(total_shift_unc)
+
+
+sum = 0
+for key in information:
+    wavelength = information[key]
+    sum += (wavelength["ratio_unc_square"])**2
+sys_unc = math.sqrt(sum)/math.sqrt(len(information))
+print(sys_unc)
+
+
+
+results = {
+    "mean" : mean,
+    "standard deviation" : std_dev,
+    "stat uncertainty" : stat_unc,
+    "sys uncertainty" : sys_unc
+}
+
+print(results)
