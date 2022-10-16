284a1df0c954adee04dfb6aa9283f1a1f747ef6c
Alpha-Kittens <90939705+Alpha-Kittens@users.noreply.github.com>
2022-10-16 14:19:54 -0400

    calibration and models updates

--- main_calibration.py
+++ main_calibration.py
@@ -6,60 +6,114 @@
 
 from data.data_loader import read_data
 from fitters import fit
-from models import *
+from models_2 import *
 from noise_reduction import reduce_noise
 import matplotlib.pyplot as plt
 import lmfit
 import numpy as np
 
 # File Path to main peak data files
-folder = "data/mercury/SuperFineScans"
+folder = "data/10.14.22/mercury/"
 
 # Define Main Peak Expected Lines
 wavelengths = {
-    'reference': [],
+    # https://physics.nist.gov/PhysRefData/Handbook/Tables/mercurytable2.htm
+    # possibly include parameter for hyperfine splitting
+    'reference': [6149.475, 5790.663, 5769.598, 5460.735, 4046.563, 3650.153],
     'check': [],
 }
 
+# errors not provided for these measurements. 
+"""
 wavelengths_errors = {
     'reference': [],
     'check' : [],
 }
+"""
 
 files = {
-    'refrence': [],
+    'reference': ["6149.50-coarse", "5790.66-superfine", "5769.6-superfine", "5460.74-superfine", "4046.56-superfine_4", "3650.15-superfine"],
     'check' : [],
 }
 
 for key in files:
-    for string in files[key]:
-        string = folder + string
+    for i in range(len(files[key])):
+        files[key][i] = folder + files[key][i]
+
+files['reference'][4] = "data/mercury/calibration_scan_9_4019_4023"
 
 # Read data
 # Noise reduction and find Uncertainties
 
+# noise-reduced version: 
+"""
 data = {
     'reference': [(reduce_noise(read_data(files['reference'][i]))) for i in range(len(files['reference']))],
     'check': [(reduce_noise(read_data(files['check'][i]))) for i in range(len(files['check']))],
 }
+"""
 
-results_reference = []
+# non noise-reduced version:
+data = {
+    'reference' : [read_data(file) for file in files['reference']],
+    'check' : [read_data(file) for file in files['check']]
+}
+
+superfine_stepsize = 0.0025
+
+# plot, user request splitting estimate
+
+measured_reference = []
 error_reference = []
 
 # Voigt Model & Fit
 for key in data:
     for entry in data[key]:
 
-        new_data, weights = entry
+        new_data, weights = reduce_noise(entry)
+
+        plt.scatter(entry[:,0], entry[:,1], marker = '.', label = "data")
+        plt.plot(entry[:,0], new_data[:,1], label = "noise-reduced")
+        plt.legend()
+        plt.show()
+
+        shift = float(input("Estimate the splitting in that plot (Angstroms): "))
+
+        if shift != 0:
+            choice = 'two_voigt'
+        else:
+            choice = 'voigt_with_shift'
         
-        model = lmfit.models.VoigtModel()
-        params = voigt_params(model, new_data)
+        model = lmfit.Model(voigt_models[choice][0])
+        if choice == 'two_voigt':
+            params = voigt_models[choice][1](entry, shift)
+        else:
+            params = voigt_models[choice][1](entry)
 
-        result = fit(model, new_data, params, weights)
+        result = fit(model, entry, params, weights)
 
-        cps, wavelength, error = extract_voigt(result)
+        extract = voigt_models[choice][2](result)
+        
+        amp, mu, alpha, gamma = extract[0][0:4]
 
-        results_reference.append(wavelength)
+        if len(extract[0]) > 4:
+            a = extract[0][4]
+        else:
+            a = 0
+
+        plt.scatter(entry[:,0], entry[:,1], marker = '.', label = "data")
+        plt.plot(entry[:,0], voigt_models['voigt_with_shift'][0](entry[:,0], amp, mu, alpha, gamma, a), label = "primary peak")
+        if choice == 'two_voigt':
+            amp2, mu2, alpha2, gamma2 = extract[0][5:9]
+            plt.plot(entry[:,0], voigt_models[choice][0](entry[:,0], amp, mu, alpha, gamma, amp2, mu2, alpha2, gamma2, a), label = "full fit")
+        plt.axvline(x = mu, label = "wavelength estimate")
+        plt.legend()
+        plt.show()
+
+        wavelength = mu
+        error = extract[1][1]
+
+        measured_reference.append(wavelength)
         error_reference.append(error)
 
 # Maximum Counts
@@ -72,9 +126,10 @@ for key in data:
 
 calibration_model = lmfit.Model(quadratic)
 calibration_error = quadratic_err
+"""
 
 inputs = np.zeros((len(results_reference), 2))
-inputs[:,0] = results_reference
+inputs[:,0] = measured_reference
 inputs[:,1] = wavelengths['reference']
 
 result = fit(model, inputs, weights = error_reference)
@@ -108,11 +163,13 @@ if __name__ == "__main__":
     plt.plot(data_check, calibration_test)
     plt.plot(data_check, wavelengths['check'])
     plt.show()
+"""
 
 #score the model/quantify performance
 
-def apply_model(wavelength_measured, wavelength_error):
-    """ 
+
+# def apply_model(wavelength_measured, wavelength_error):
+""" 
     Applies the found calibration model. Can be imported by other functions. 
 
     Arguments: 
@@ -123,6 +180,6 @@ def apply_model(wavelength_measured, wavelength_error):
         * As a 2-tuple:
             - `wavelength` (float): corrected wavelength
             - `wavelength_error` (float): error on corrected wavelength
-    """
+"""
     
-    return calibration(wavelength_measured), calibration_error(wavelength_measured, wavelength_error)
+#    return calibration(wavelength_measured), calibration_error(wavelength_measured, wavelength_error)
---
+++ models_2.py
@@ -0,0 +1,193 @@
+import numpy as np
+import lmfit
+from scipy.special import wofz
+from max_model import regions
+
+def linear(x, m, b):
+    """
+    linear model y=mx+b
+
+    Arguments: 
+        * `x` : parameter value to evaluate the function at
+        * `m` : slope of the line
+        * `b` : y-intercept of the line
+
+    Returns: 
+        * `y` result of y=mx+b
+    """
+    return (m*x)+b
+
+def linear_extract(result):
+
+    pass
+
+def quadratic(x, a, b, c):
+    """
+    quadratic model y=ax^2 + bx + c
+
+    Arguments: 
+        * `x` : parameter value to evaluate the function at
+        * `a` : leading coefficient
+        * `b` : 2nd term coefficient
+        * `c` : y-interecept
+
+    Returns: 
+        * `y` : result of y=ax^2 + bx + c
+    """
+    return (a*(x**2)) + (b*x) + c
+
+def quadratic_extract(result):
+
+    pass
+
+def quadratic_err(x, x_err, a, a_err, b, b_err, c, c_err):
+    """
+    Error for result of quadratic model
+
+    Arguments:
+        * `x` : parameter value to evaluate function
+        * `x_err` : error associated with `x`
+        * `a` : leading coefficient
+        * `a_err` : error associated with `a`
+        * `b` : linear coefficient
+        * `b_err` : error associated with `b`
+        * `c` : y-intercept
+        * `c_err` : error associated with `c`
+    """
+
+    x_rel = x_err / x
+    a_rel = a_err / a
+    b_rel = b_err / b
+    c_rel = c_err / c
+
+    return ((a * x**2)**2 * (a_rel**2 + 2 * x_rel**2) + (b * x)**2 * (b_rel ** 2 + x_rel**2) + c_err ** 2) ** (1/2)
+
+def voigt(x, amp, mu, alpha, gamma):
+    """
+    Voigt, i.e. gaussian convolved with lorentzian
+
+    Argumetns:
+        * `x` : parameter value to evaluate the function at
+        * `amp` : amplitude of distribution
+        * `mu` : center of distribution
+        * `alpha` : Half-width half-maximum of Gaussian (monotonic with sigma)
+        * 'gamma` : Half-width half-maximum of Lorentzian
+
+    Returns:
+        * `y` : evaluation of voigt model at x at given parameters
+    """
+
+    sigma = alpha / np.sqrt(2 * np.log(2))
+
+    return amp * np.real(wofz((x - mu + 1j*gamma)/sigma/np.sqrt(2))) / sigma / np.sqrt(2*np.pi)
+
+def voigt_extract(result):
+
+    return ((result.params['amp'].value, result.params['mu'].value, result.params['alpha'].value, result.params['gamma'].value), (result.params['amp'].stderr, result.params['mu'].stderr, result.params['alpha'].stderr, result.params['gamma'].stderr))
+
+voigt_with_shift = lambda x, amp, mu, alpha, gamma, a : voigt(x, amp, mu, alpha, gamma) + a
+
+def voigt_with_shift_extract(result):
+
+    return ((result.params['amp'].value, result.params['mu'].value, result.params['alpha'].value, result.params['gamma'].value, result.params['a'].value), (result.params['amp'].stderr, result.params['mu'].stderr, result.params['alpha'].stderr, result.params['gamma'].stderr, result.params['a'].stderr))
+
+two_voigt = lambda x, amp1, mu1, alpha1, gamma1, amp2, mu2, alpha2, gamma2, a : voigt(x, amp1, mu1, alpha1, gamma1) + voigt(x, amp2, mu2, alpha2, gamma2) + a
+
+def two_voigt_extract(result):
+
+    return ((result.params['amp1'].value, result.params['mu1'].value, result.params['alpha1'].value, result.params['gamma1'].value, result.params['a'].value, result.params['amp2'].value, result.params['mu2'].value, result.params['alpha2'].value, result.params['gamma2'].value), (result.params['amp1'].stderr, result.params['mu1'].stderr, result.params['alpha1'].stderr, result.params['gamma1'].stderr, result.params['a'].stderr, result.params['amp2'].stderr, result.params['mu2'].stderr, result.params['alpha2'].stderr, result.params['gamma2'].stderr))
+
+def get_voigt_params(data, return_max = False):
+
+    wavelengths, cps = data[:,0], data[:,1]
+    
+    signals = regions(cps)[1]
+
+    maxes = [max(cps[signal[0]:signal[1]+1]) for signal in signals]
+
+    max_signal = signals[np.argmax(maxes)]
+    arg_max = np.argmax(cps)
+    start_mu, start_amp = wavelengths[arg_max], cps[arg_max]
+    print (start_mu)
+    print (start_amp)
+    start_alpha = (wavelengths[max_signal[1]] - wavelengths[max_signal[0]]) / 4
+    start_gamma = start_alpha / 4
+    print (start_alpha)
+    print (start_gamma)
+    start_amp *= start_amp / max(voigt(data[:,0], start_amp, start_mu, start_alpha, start_gamma))
+
+    if return_max:
+        return start_amp, start_mu, start_alpha, start_gamma, arg_max
+    return start_amp, start_mu, start_alpha, start_gamma
+
+def voigt_params(data, return_max = False):
+
+    params = lmfit.Parameters()
+
+    start_amp, start_mu, start_alpha, start_gamma = get_voigt_params(data, return_max = return_max)
+
+    import matplotlib.pyplot as plt
+
+    #plt.scatter(data[:,0], data[:,1])
+    #plt.plot(data[:,0], voigt(data[:,0], start_amp, start_mu, start_alpha, start_gamma) + min(data[:,1]))
+    #plt.show()
+
+    params.add('amp', value = start_amp, min = 0)
+    params.add('mu', value = start_mu)
+    params.add('alpha', value = start_alpha, min = 0)
+    params.add('gamma', value = start_gamma, min = 0)
+
+    return params
+
+
+def voigt_shift_params(data):
+
+    params = voigt_params(data)
+    params.add('a', min(data[:,1]))
+
+    return params
+
+def two_voigt_params(data, expected_shift, stepsize = 0.0025):
+
+    params = lmfit.Parameters()
+
+    start_amp, start_mu, start_alpha, start_gamma, arg_max = get_voigt_params(data, return_max = True)
+
+    if arg_max + int(expected_shift // stepsize) > len(data[:,1]):
+        start_amp2 = data[:,1][max(arg_max - int(expected_shift // stepsize), 0)]
+        start_mu2 = data[:,0][max(arg_max - int(expected_shift // stepsize), 0)]
+    elif arg_max - int(expected_shift // stepsize) < 0:
+        start_amp2 = data[:,1][arg_max + int(expected_shift // stepsize)]
+        start_mu2 = data[:,0][arg_max + int(expected_shift // stepsize)]        
+    elif data[:,1][arg_max + int(expected_shift // stepsize)] > data[:,1][arg_max - int(expected_shift // stepsize)]:
+        start_amp2 = data[:,1][arg_max + int(expected_shift // stepsize)]
+        start_mu2 = data[:,0][arg_max + int(expected_shift // stepsize)]
+    else:
+        start_amp2 = data[:,1][arg_max - int(expected_shift // stepsize)]
+        start_mu2 = data[:,0][arg_max - int(expected_shift // stepsize)]
+    start_amp2 *= start_amp2 / max(voigt(data[:,0], start_amp2, start_mu2, start_alpha, start_gamma))
+
+    params.add('amp1', value = start_amp, min = 0)
+    params.add('mu1', value = start_mu)
+    params.add('alpha1', value = start_alpha, min = 1e-6)
+    params.add('gamma1', value = start_gamma, min = 0)
+    params.add('amp2', value = start_amp2, min = 0)
+    params.add('mu2', value = start_mu2, min = 1e-6)
+    params.add('alpha2', value = start_alpha, min = 0)
+    params.add('gamma2', value = start_gamma)
+    params.add('a', min(data[:,1]))
+
+    return params
+
+voigt_models = {
+    'voigt' : (voigt, voigt_params, voigt_extract),
+    'voigt_with_shift' : (voigt_with_shift, voigt_shift_params, voigt_with_shift_extract),
+    'two_voigt' : (two_voigt, two_voigt_params, two_voigt_extract),
+}
+
+if __name__ == '__main__':
+    import matplotlib.pyplot as plt
+    x = np.linspace(5748.5, 5750.5, 100)
+    plt.plot(x, voigt(x, 1144, 5749, 0.05, 0.05))
+    print(max(voigt(x, 1144, 5749, 0.05, 0.05)))
+    plt.show()
